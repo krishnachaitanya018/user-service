63ffec15319134ae0e237ceff41d3fb96129eaa4
krishnachaitanya018 <chaitanyaganni018@gmail.com>
2025-10-28 18:04:53 -0400

    added flask-based version in python

--- src/app.py
+++ src/app.py
@@ -1,1 +1,11 @@
-print("User Service Running")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "User Service Running"
+
+if __name__ == '__main__':
+    # Keeps the process running
+    app.run(host='0.0.0.0', port=8080)
