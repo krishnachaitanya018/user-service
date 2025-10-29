78651fc63c653bef05368c3a4f88f308d4d931a2
krishnachaitanya018 <chaitanyaganni018@gmail.com>
2025-10-29 12:01:06 -0400

    updated python code

--- src/app.py
+++ src/app.py
@@ -1,11 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "User Service Running"
-
-if __name__ == '__main__':
-    # Keeps the process running
-    app.run(host='0.0.0.0', port=8080)
+print("User Service Running")
